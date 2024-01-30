7e0275340490826dd8ebce08ec7f4b807fef6974
Callum Young <callum.young@ukho.gov.uk>
2024-01-30 12:42:29 +0000

    update eventhub name

--- eventhub.tf
+++ eventhub.tf
@@ -1,5 +1,5 @@
 resource "azurerm_eventhub" "logging" {
-  name                = "${var.product_alias}-${var.env}-evhns-logging-test"
+  name                = "${var.product_alias}-${var.env}-evh-logging-test"
   namespace_name      = azurerm_eventhub_namespace.this.name
   resource_group_name = var.resource_group_name
   partition_count     = 2
