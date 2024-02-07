10ffe2db6098aed1983645f314327a63ce1d32ef
Callum Young <callum.young@ukho.gov.uk>
2024-02-07 14:28:39 +0000

    push

--- elastic-azure-log-intergration.tf
+++ elastic-azure-log-intergration.tf
@@ -39,7 +39,7 @@ resource "null_resource" "create_agent_policy" {
   }
 }
 
-resource "null_resource" "create_package_policy3" {
+resource "null_resource" "create_package_policy4" {
   # Your other resource configuration here...
 
   provisioner "local-exec" {
