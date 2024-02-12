bf5f191aaf8221df6cc34efc5764d94d59786f35
Callum Young <callum.young@ukho.gov.uk>
2024-02-12 10:26:18 +0000

    test

--- elastic-azure-log-intergration.tf
+++ elastic-azure-log-intergration.tf
@@ -186,7 +186,7 @@ resource "null_resource" "create_package_policy" {
     "consumer_group": "$Default",
     "connection_string": "${var.event_hub_namespace_connection_string}",
     "storage_account": "${var.ELK_storage_account}",
-    "storage_account_key": "${var.ELK_storage_account_key}"
+    "storage_account_key": "${var.ELK_storage_account}"
   }
 }'
     EOT
