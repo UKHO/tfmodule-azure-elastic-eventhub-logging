7a40fffa75c6670d5ea4754b41cc24b001a1a7f2
Callum Young <callum.young@ukho.gov.uk>
2024-02-14 12:14:11 +0000

    create evh auth rule

--- eventhub.tf
+++ eventhub.tf
@@ -5,3 +5,14 @@ resource "azurerm_eventhub" "logging" {
   partition_count     = 2
   message_retention   = 1
 }
+
+resource "azurerm_eventhub_authorization_rule" "root_manage" {
+  name                = "RootManageSharedAccessKey"
+  namespace_name      = var.event_hub_namespace
+  resource_group_name = var.resource_group_name
+  eventhub_name       = azurerm_eventhub.logging.name  # Replace with your Event Hub name
+  listen              = true
+  send                = true
+  manage              = true
+  depends_on = [ azurerm_eventhub.logging ]
+}
