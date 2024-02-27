32fe9d3b344bd40feeb3cce6be034dc0bf9ac019
Callum Young <callum.young@ukho.gov.uk>
2024-02-27 16:34:01 +0000

    lower case evh name

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  eventhub_name = azurerm_eventhub.logging.name
+  eventhub_name = lower(azurerm_eventhub.logging.name)
   eventhub_connection_string = azurerm_eventhub_authorization_rule.root_manage.primary_connection_string
 }
