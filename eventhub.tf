bdf4b90a05b2f7353e275657f1f0ddb11d5de330
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2025-04-25 16:33:28 +0100

    Remove RootManage from hub

--- eventhub.tf
+++ eventhub.tf
@@ -6,17 +6,6 @@ resource "azurerm_eventhub" "logging" {
   message_retention   = 1
 }
 
-resource "azurerm_eventhub_authorization_rule" "root_manage" {
-  name                = "RootManageSharedAccessKey"
-  namespace_name      = var.event_hub_namespace
-  resource_group_name = var.resource_group_name
-  eventhub_name       = azurerm_eventhub.logging.name
-  listen              = true
-  send                = true
-  manage              = true
-  depends_on = [ azurerm_eventhub.logging ]
-}
-
 resource "azurerm_eventhub_authorization_rule" "logging_listen" {
   name                = "elastic-listener"
   namespace_name      = var.event_hub_namespace
