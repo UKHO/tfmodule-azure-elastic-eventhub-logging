62153558ade19a03e20819da67e693bfd958949a
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2025-04-25 16:37:43 +0100

    maybe need a send rule?

--- eventhub.tf
+++ eventhub.tf
@@ -16,3 +16,14 @@ resource "azurerm_eventhub_authorization_rule" "logging_listen" {
   manage              = false
   depends_on = [ azurerm_eventhub.logging ]
 }
+
+resource "azurerm_eventhub_authorization_rule" "logging_send" {
+  name                = "log-send"
+  namespace_name      = var.event_hub_namespace
+  resource_group_name = var.resource_group_name
+  eventhub_name       = azurerm_eventhub.logging.name
+  listen              = false
+  send                = true
+  manage              = false
+  depends_on = [ azurerm_eventhub.logging ]
+}
