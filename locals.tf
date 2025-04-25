7688d460143a1508e4e0b9591c8b766c50eaca35
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2025-04-25 16:39:06 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
   eventhub_name = lower(azurerm_eventhub.logging.name)
   eventhub_connection_string = azurerm_eventhub_authorization_rule.logging_listen.primary_connection_string
+  eventhub_connection_string_send = azurerm_eventhub_authorization_rule.logging_send.primary_connection_string
 }
