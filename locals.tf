94551820e8f879bc0f013a1f2136163a0db92733
Luke Duddridge <205003513+ld-at-work@users.noreply.github.com>
2025-06-26 10:22:09 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals {
   eventhub_name = lower(azurerm_eventhub.logging.name)
-  eventhub_connection_string = azurerm_eventhub_authorization_rule.logging_listen.primary_connection_string
   eventhub_connection_string_send = azurerm_eventhub_authorization_rule.logging_send.primary_connection_string
 }
