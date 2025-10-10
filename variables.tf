5e1ce35e1dd33035a50c7fcc8ae958ecb6baad67
samtitmuss <sam.titmuss@ukho.gov.uk>
2025-10-10 10:20:37 +0100

    updating

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "name" {
-  type = string
-  default = "test"
-}
-
 variable "product_alias" {
   type = string
   description = "The alias for the project"
@@ -18,11 +13,6 @@ variable "env" {
   default = "Environment name or resource"
 }
 
-variable "location" {
-  type = string
-  description = "Location the resource will run in"
-}
-
 variable "resource_group_name" {
   type = string
   description = "Resource Group name"
@@ -31,18 +21,6 @@ variable "resource_group_name" {
 variable "event_hub_namespace" {}
 
 variable "configuration_store_id" {}
-
-variable "agent_policy_id" {}
-
-variable "elk_api_key" {}
-
-variable "elk_url" {}
-
-variable "elk_storage_account" {}
-
-variable "elk_storage_account_key" {}
-
-variable "elk_namespace" {} # must be lower case
 
 variable "svc_min_log_level" {}
 
